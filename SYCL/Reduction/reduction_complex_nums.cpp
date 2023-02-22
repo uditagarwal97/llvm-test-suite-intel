0d1a4777b30d69f075d81d0a21cda076a1a31a0c
Agarwal, Udit <udit.agarwal@intel.com>
2023-02-22 08:33:55 -0800

    Fix formatting.

--- SYCL/Reduction/reduction_complex_nums.cpp
+++ SYCL/Reduction/reduction_complex_nums.cpp
@@ -17,24 +17,22 @@ using namespace sycl;
 template <typename T>
 void test_identityless_reduction_for_complex_nums(queue &q) {
   // Allocate and initialize buffer on the host with all 1's.
-  buffer<std::complex<T>> valuesBuf { 1024 };
+  buffer<std::complex<T>> valuesBuf{1024};
   {
-    host_accessor a { valuesBuf };
+    host_accessor a{valuesBuf};
     std::fill(a.begin(), a.end(), std::complex<T>(1));
   }
 
   // Buffer to hold the reduction results.
   std::complex<T> sumResult = 0;
-  buffer<std::complex<T>> sumBuf { &sumResult, 1 };
+  buffer<std::complex<T>> sumBuf{&sumResult, 1};
 
-  q.submit([&](handler& cgh) {
+  q.submit([&](handler &cgh) {
     auto inputVals = valuesBuf.template get_access<access_mode::read>(cgh);
     auto sumReduction = reduction(sumBuf, cgh, plus<std::complex<T>>());
 
-    cgh.parallel_for(range<1> {1024}, sumReduction,
-                     [=](id<1> idx, auto& sum) {
-      sum += inputVals[idx];
-    });
+    cgh.parallel_for(range<1>{1024}, sumReduction,
+                     [=](id<1> idx, auto &sum) { sum += inputVals[idx]; });
   });
 
   assert(sumBuf.get_host_access()[0] == static_cast<T>(1024));
