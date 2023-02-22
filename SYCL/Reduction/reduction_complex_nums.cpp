3c99d052eabee2d1f84e5491cc44e01702973f7c
Agarwal, Udit <udit.agarwal@intel.com>
2023-02-22 11:06:20 -0800

    Minor changes.

--- SYCL/Reduction/reduction_complex_nums.cpp
+++ SYCL/Reduction/reduction_complex_nums.cpp
@@ -20,7 +20,9 @@ void test_identityless_reduction_for_complex_nums(queue &q) {
   buffer<std::complex<T>> valuesBuf{1024};
   {
     host_accessor a{valuesBuf};
-    std::fill(a.begin(), a.end(), std::complex<T>(1));
+    T n = 0;
+    std::generate(a.begin(), a.end(),
+                  [&n] { return std::complex<T>(n, ++n + 1); });
   }
 
   // Buffer to hold the reduction results.
@@ -28,14 +30,14 @@ void test_identityless_reduction_for_complex_nums(queue &q) {
   buffer<std::complex<T>> sumBuf{&sumResult, 1};
 
   q.submit([&](handler &cgh) {
-    auto inputVals = valuesBuf.template get_access<access_mode::read>(cgh);
+    accessor inputVals{valuesBuf, cgh, sycl::read_only};
     auto sumReduction = reduction(sumBuf, cgh, plus<std::complex<T>>());
 
     cgh.parallel_for(range<1>{1024}, sumReduction,
                      [=](id<1> idx, auto &sum) { sum += inputVals[idx]; });
   });
 
-  assert(sumBuf.get_host_access()[0] == static_cast<T>(1024));
+  assert(sumBuf.get_host_access()[0] == std::complex<T>(523776, 525824));
 }
 
 int main() {
